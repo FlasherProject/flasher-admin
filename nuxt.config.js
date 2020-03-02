d82f928f3e11e29e66cc2105e5f5738e8c0f41ef
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2020-03-02 12:20:36 +0100

    add typescript support

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
+    '@nuxt/typescript-build',
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
