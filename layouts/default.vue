5d6ec3da4f79ef0a2d04eb28621d46387d9cb2d5
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2020-03-02 13:07:24 +0100

    add sidebar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,18 +19,7 @@
     </nav>
 
     <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
+      <Sidebar></Sidebar>
 
       <div class="container column is-10">
         <nuxt />
@@ -40,7 +29,12 @@
 </template>
 
 <script>
+import Sidebar from '~/components/Sidebar'
+
 export default {
+  components: {
+    Sidebar
+  },
   data() {
     return {
       items: [
