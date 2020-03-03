a224d792a9ebd8bbeadb3e81e25595bece091a1b
Antoine Gravelot <antoine.gravelot@hotmail.fr>
2020-03-03 13:31:33 +0100

    remove unused navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,5 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28">
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
     <section class="main-content columns">
       <sidebar />
 
@@ -34,22 +16,6 @@ import Sidebar from '~/components/Sidebar'
 export default {
   components: {
     Sidebar
-  },
-  data () {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
   }
 }
 </script>
