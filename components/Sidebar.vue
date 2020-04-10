a6f8a1f3eff8c68456b561f30628e252438b69dc
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2020-04-10 12:15:49 +0200

    cleanup

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -66,6 +66,8 @@
           icon="cog"
           tag="nuxt-link"
         />
+      </b-menu-list>
+      <b-menu-list label="Others">
         <b-menu-item
           href="https://jkanda.fr"
           label="Return to website"
@@ -85,68 +87,3 @@ class Sidebar extends Vue {}
 
 export default Sidebar
 </script>
-
-<style lang="scss">
-  @media screen and (max-width: 1023px) {
-    .b-sidebar {
-      .sidebar-content {
-        &.is-mini-mobile {
-          &:not(.is-mini-expand),
-          &.is-mini-expand:not(:hover) {
-            .menu-list {
-              li {
-                a {
-                  span:nth-child(2) {
-                    display: none;
-                  }
-                }
-                ul {
-                  padding-left: 0;
-                  li {
-                    a {
-                      display: inline-block;
-                    }
-                  }
-                }
-              }
-            }
-            .menu-label:not(:last-child) {
-              margin-bottom: 0;
-            }
-          }
-        }
-      }
-    }
-  }
-  @media screen and (min-width: 1024px) {
-    .b-sidebar {
-      .sidebar-content {
-        &.is-mini {
-          &:not(.is-mini-expand),
-          &.is-mini-expand:not(:hover) {
-            .menu-list {
-              li {
-                a {
-                  span:nth-child(2) {
-                    display: none;
-                  }
-                }
-                ul {
-                  padding-left: 0;
-                  li {
-                    a {
-                      display: inline-block;
-                    }
-                  }
-                }
-              }
-            }
-            .menu-label:not(:last-child) {
-              margin-bottom: 0;
-            }
-          }
-        }
-      }
-    }
-  }
-</style>
